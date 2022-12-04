a06e91af13ad9db5529699d48c849b7648967d16
Muhammad Najib Robbani <najibrobbani74@gmail.com>
2022-12-04 08:56:24 +0700

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: PageOne(),
+    title: "Appku",
+    home: Home(),
   ));
 }
 
-class PageOne extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Container(
-        color: Colors.black54,
-        width: 200,
-        height: 200,
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-                "Helloo",
-                style: TextStyle(
-                    color: Colors.greenAccent,
-                    fontFamily: "Serif",
-                    fontSize: 30),
-              ),
-              Icon(Icons.favorite)
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        leading: Icon(Icons.home),
+        title: Center(
+          child: Text("Home"),
         ),
+        actions: [Icon(Icons.search)],
       ),
-    ));
+    );
   }
 }
