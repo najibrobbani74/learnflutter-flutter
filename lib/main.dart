12c9621822d859addeb91e51608621127c40555f
Muhammad Najib Robbani <najibrobbani74@gmail.com>
2022-12-04 08:46:21 +0700

    TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HelloWorld(),
+    home: PageOne(),
   ));
 }
 
-class HelloWorld extends StatelessWidget {
+class PageOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello World"),
+        body: Center(
+      child: Container(
+        color: Colors.black54,
+        width: 200,
+        height: 200,
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                "Helloo",
+                style: TextStyle(
+                    color: Colors.greenAccent,
+                    fontFamily: "Serif",
+                    fontSize: 30),
+              ),
+              Icon(Icons.favorite)
+            ],
+          ),
+        ),
       ),
-    );
+    ));
   }
 }
