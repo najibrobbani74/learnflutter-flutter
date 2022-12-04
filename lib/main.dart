5db483996cd26ef5cef40f3c35be04ef2b4cb66b
Muhammad Najib Robbani <najibrobbani74@gmail.com>
2022-12-04 09:21:46 +0700

    CardParsing

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,44 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        leading: Icon(Icons.home),
-        title: Center(
-          child: Text("Home"),
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          leading: Icon(Icons.home),
+          title: Center(
+            child: Text("Card"),
+          ),
+          actions: [Icon(Icons.search)],
         ),
-        actions: [Icon(Icons.search)],
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Icon(Icons.cabin),
-              Icon(Icons.home),
-            ],
-          ),
-          Row(
-            children: [
-              Icon(Icons.cabin),
-              Icon(Icons.home),
-            ],
-          ),
-        ],
+        body: Column(
+          children: [
+            CardRumah(Icons.home_repair_service_sharp, "hallo"),
+            CardRumah(Icons.home_repair_service_sharp, "haaallo"),
+            CardRumah(Icons.home_repair_service_sharp, "halfflo"),
+          ],
+        ));
+  }
+}
+
+class CardRumah extends StatelessWidget {
+  CardRumah(this.icon, this.text);
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Card(
+        child: Column(
+          children: [
+            Center(
+              child: Icon(icon),
+            ),
+            Center(
+              child: Text(text),
+            ),
+          ],
+        ),
       ),
     );
   }
